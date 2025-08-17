7a0f1889bf5c4ed0b21d8e00e4eb365172419e12
rivancorp <52347916+rivancorp@users.noreply.github.com>
2025-08-17 13:54:44 +0800

    Update createDNSrecords.ps1

--- createDNSrecords.ps1
+++ createDNSrecords.ps1
@@ -1,6 +1,8 @@
 ﻿Install-WindowsFeature  -name dns  -includeManagementTools
 Add-DnsServerPrimaryZone -Name "ngcpM.ph" -ZoneFile "ngcpM.ph.dns"
 add-DnsServerResourceRecord -zonename ngcpM.ph -A -name ns  -ipv4address 10.m.1.10
+add-DnsServerResourceRecord -zonename ngcp12.com -A -name "." -ipv4address 10.12.1.10
+#
 add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name www -hostname ns.ngcpM.ph
 add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name imap -hostname ns.ngcpM.ph
 add-DnsServerResourceRecord -zonename ngcpM.ph -Cname -name pop -hostname ns.ngcpM.ph
@@ -14,7 +16,9 @@ add-DnsServerResourceRecord -zonename ngcpM.ph -A -name p1  -ipv4address 10.m.10
 add-DnsServerResourceRecord -zonename ngcpM.ph -A -name p2  -ipv4address 10.m.100.102
 add-DnsServerResourceRecord -zonename ngcpM.ph -A -name c1  -ipv4address 10.m.50.6
 add-DnsServerResourceRecord -zonename ngcpM.ph -A -name c2  -ipv4address 10.m.50.8
-
 add-DnsServerResourceRecord -zonename ngcpM.ph -A -name ap  -ipv4address 10.m.10.3
 #
+﻿#creating all important zones:
+#
 Add-DnsServerResourceRecordMX -Preference 10  -Name "." -MailExchange "ns.ngcp12.com" -ZoneName "ngcp12.com"
+
